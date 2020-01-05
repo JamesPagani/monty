0771b1af1ffff35df30e0054b21b120f6753a694
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-05 00:04:51 +0000

    Essential operations done in a Stack data structure

---
+++ stack_ops.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+/**
+ * push - Adds a new element to the top of the stack
+ * @stack: The stack itself
+ * @line_number: Monty file's line number (used for error handling)
+ * Return: Nothing
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *strint;
+	stack_t *new;
+
+	strint = malloc(12 * sizeof(char));
+	new = malloc(sizeof(stack_t));
+	if (strint == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_list();
+		exit(EXIT_FAILURE);
+	}
+	strint = strtok(NULL, " ");
+	if (!(valid_int(strint)))
+	{
+		fprintf(stderr, "L%ud: usage: push integer\n", line_number);
+		free_list();
+		exit(EXIT_FAILURE);
+	}
+	new->n = atoi(strint);
+	new->prev = NULL;
+	new->next = *stack;
+	(*stack)->prev = new;
+	*stack = new;
+}
+
